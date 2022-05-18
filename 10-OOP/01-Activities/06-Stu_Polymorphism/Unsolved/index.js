a58a64f2ff84d0fd875a1d4a03331ab315f6eacd
Trevor Ridley <trevor_ridley@yahoo.com>
2022-05-17 22:09:57 -0700

    tuesday stuff!

--- 10-OOP/01-Activities/06-Stu_Polymorphism/Unsolved/index.js
+++ 10-OOP/01-Activities/06-Stu_Polymorphism/Unsolved/index.js
@@ -17,6 +17,24 @@ function Student(first, last, age) {
     // Ex. 95 => 'A'
     if (typeof input === 'number') {
       // TODO: Add logic here to return a single letter grade
+      if ( input >= 90) {
+        response = "A";
+      } else  if (input >= 70) {
+        response = "C";
+      };
+
+      // if (inRange(input, 80, 89)) {
+      //   response = "B";
+      // };
+
+      // if (input >= 70) {
+      //   response = "C";
+      // };
+
+      // else {
+      //   response = "failure";
+      // }
+
       return response;
     }
     // Return a range if a letter grade was passed
@@ -29,5 +47,5 @@ function Student(first, last, age) {
 }
 
 const John = new Student('John', 'Appleseed', '30');
-console.log('John.displayGrade():', John.displayGrade(95));
+console.log('John.displayGrade():', John.displayGrade(72));
 console.log('John.displayGrade():', John.displayGrade('B'));

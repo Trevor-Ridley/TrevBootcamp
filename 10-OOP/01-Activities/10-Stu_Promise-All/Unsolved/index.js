7e0d914a90ea9fa71baac4055388f606f4e65382
Trevor Ridley <trevor_ridley@yahoo.com>
2022-05-19 22:38:13 -0700

    healer vs barbarian!

--- 10-OOP/01-Activities/10-Stu_Promise-All/Unsolved/index.js
+++ 10-OOP/01-Activities/10-Stu_Promise-All/Unsolved/index.js
@@ -4,8 +4,24 @@ const maxDuration = 9999;
 const callAPI = (duration) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
+
+
       // TODO: If the duration is longer than maxDuration, reject() the promise
-      // TODO: Otherwise resolve() the promise
+
+
+      if (duration > maxDuration) => {
+        reject(`too long ${maxDuration} a milli`);
+      }
+
+
+       // TODO: Otherwise resolve() the promise
+
+
+      else {
+        resolve(`gucci, API call blah ${duration} long`);
+
+      }
+
     }, duration);
   });
 
@@ -14,3 +30,6 @@ const promises = apiCallDurations.map((duration) => callAPI(duration));
 console.log('Promises array before the timeouts have finished: ', promises);
 
 // TODO: Use Promise.all() to capture when the array of promises has been resolved or if any of them were rejected
+
+Promise.all(apiCallDurations)
+.then()
